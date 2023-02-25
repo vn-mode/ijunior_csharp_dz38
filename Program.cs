c5ba3f58a56d7cb5332a16bef1546b9ae3869780
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-02-25 17:30:45 +0300

    ijunior_csharp_dz38

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Виталий", 31, 100);
+            player1.ShowInfo();
+        }
+
+        class Player
+        {
+            private string _name;
+            private int _age;
+            private int _health;
+
+            public Player(string name, int age, int health)
+            {
+                _name = name;
+                _age = age;
+                _health = health;
+            }
+
+            public void ShowInfo()
+            {
+                Console.Write($"Имя игрока: {_name}\nВозраст игрока: {_age}\nЗдоровье игрока: {_health}");
+            }
+        }
+    }
+}
+
